fba3148ebcb52c76e381d4038148628252355501
EslavaDev <danieleslava52@gmail.com>
2018-09-25 20:47:54 -0500

    v1 routes

--- server/routes.js
+++ server/routes.js
@@ -9,20 +9,22 @@ const client = require('../api/client');
 const worker = require('../api/worker');
 const login = require('../api/login');
 const category = require('../api/category');
-const product = require('../api/servicePost');
+const servicePost = require('../api/servicePost');
 const uploads = require('../api/uploads');
 const images = require('../api/images');
+const quotation = require('../api/quotation');
 
 
 module.exports = (app) => {
   // Insert routes below
-  app.use('/client', client);
-  app.use('/worker', worker);
-  app.use('/login', login);
-  app.use('/category', category);
-  app.use('/product', product);
-  app.use('/uploads', uploads);
-  app.use('/images', images);
+  app.use('/v1/client', client);
+  app.use('/v1/worker', worker);
+  app.use('/v1/login', login);
+  app.use('/v1/category', category);
+  app.use('/v1/Post', servicePost);
+  app.use('/v1/uploads', uploads);
+  app.use('/v1/images', images);
+  app.use('/v1/quotation', quotation);
 
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
