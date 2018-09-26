b0f4f6ae02effea7dbe5a7cf0fd4ba790eb76681
EslavaDev <danieleslava52@gmail.com>
2018-09-25 20:56:45 -0500

    v1 routes

--- api/client/index.js
+++ api/client/index.js
@@ -1,12 +1,12 @@
 const { Router } = require('express');
 const controller = require('./client.controller');
-const { verifyToken, verifyAdminRole } = require('../../server/middlewares/auth');
+const { verifyToken } = require('../../server/middlewares/auth');
 
 const router = new Router();
 
 router.get('/', verifyToken, controller.getUsers);
-router.post('/', [verifyToken, verifyAdminRole], controller.saveUser);
-router.put('/:id', [verifyToken, verifyAdminRole], controller.updateUser);
-router.delete('/:id', [verifyToken, verifyAdminRole], controller.deleteUser);
+router.post('/', controller.saveUser);
+router.put('/:id', [verifyToken], controller.updateUser);
+router.delete('/:id', [verifyToken], controller.deleteUser);
 
 module.exports = router;
