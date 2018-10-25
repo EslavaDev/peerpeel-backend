f66cd438269dd0271ec4ca4003988fe0e08a9b86
EslavaDev <danieleslava52@gmail.com>
2018-10-25 00:19:54 -0500

    schema change servicePost

--- api/servicePost/index.js
+++ api/servicePost/index.js
@@ -10,7 +10,7 @@ router.put('/:id', [verifyToken], controller.updatedService);
 router.get('/:id', verifyToken, controller.getId);
 router.get('/search/:term', verifyToken, controller.getServicePostSearch);
 router.get('/search/:id/:term', verifyToken, controller.getServicePostSearchCategory);
-router.get('/', [verifyToken, verifyWorkerRole], controller.getAll);
+router.get('/', [verifyToken], controller.getAll);
 router.get('/category/:id', [verifyToken, verifyWorkerRole], controller.getAllForCategory);
 router.delete('/:id', [verifyToken, verifyClientRole], controller.removeService);
 
