f1f34ccae76a5df499e00409ecefaac67ea41cef
EslavaDev <danieleslava52@gmail.com>
2018-12-19 22:03:20 -0500

    add app web

--- api/service/service.controller.js
+++ api/service/service.controller.js
@@ -1,7 +1,27 @@
 const Service = require('./service.service');
 
 exports.create = (req, res) => {
-  Service.SavedDataBasePromise(req.body)
+  const {
+    date,
+    hour,
+    client,
+    phone,
+    anotation,
+    type,
+    address,
+    sector,
+  } = req.body;
+  const data = {
+    client,
+    date,
+    hour,
+    phone,
+    anotation,
+    address,
+    sector,
+    type,
+  };
+  Service.SavedDataBasePromise(data)
     .then(response => res.status(201).json(response))
     .catch(error => res.status(400).json(error));
   // id = `${Date.now()}PEER${Math.floor((Math.random()*100)+1)}`;
