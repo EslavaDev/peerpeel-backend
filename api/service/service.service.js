8dcdd3eec58b4cb6e966b2d0a3f1e17705d9ef6b
EslavaDev <danieleslava52@gmail.com>
2018-12-19 21:50:42 -0500

    add app web

--- api/service/service.service.js
+++ api/service/service.service.js
@@ -7,12 +7,18 @@ exports.SavedDataBasePromise = data => new Promise((resolve, reject) => {
     .set(data)
     .then((res) => {
       console.log(data, res);
-      return resolve({ ok: true, data, message: `SUCCESS ${data.user}` });
+      resolve({ ok: true, data, message: `SUCCESS ${data.client}` });
       // res.status(200).json({ ok: true, message: "SUCCESS " + auth.uid, auth: auth });
+      return 1;
     })
     .catch((error) => {
       console.log(error);
-      return reject(error);
+      const temp = {
+        error,
+        message: 'putito',
+      }
+      reject(temp);
+      return 1;
       // res.status(400).json(error);
     });
 });
